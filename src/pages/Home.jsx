14bf930d66596f4ef7f51bd1eda625a297e3feae
Lucky Kumar <lucky9110132241@gmail.com>
2025-09-14 04:38:05 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Speciality from '../components/Speciality'
+import TopDoctors from '../components/TopDoctors'
+
+const Home = () => {
+  return (
+    <div>
+     <Header/>
+     <Speciality/>
+     <TopDoctors/>
+    </div>
+  )
+}
+
+export default Home
